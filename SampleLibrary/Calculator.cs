bc29651209d1ae2cca36caa4778fc8554d4902e6
aykutmayali <aykutmayali@gmail.com>
2020-11-04 14:39:26 +0300

    first commit

---
+++ SampleLibrary/Calculator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace SampleLibrary
+{
+    public class Calculator : ICalculator
+    {
+        public int FirstNumber { get; set; }
+        public int SecondNumber { get; set; }
+        public int Result { get; set; }
+
+        public void Add()
+        {
+            Result = FirstNumber + SecondNumber;
+        }
+
+        public void Subtract()
+        {
+            Result = FirstNumber - SecondNumber;
+        }
+
+        public void Multiply()
+        {
+            Result = FirstNumber * SecondNumber;
+        }
+
+        public void Divide()
+        {
+            Result = FirstNumber / SecondNumber;
+        }
+
+        public void CleanAll()
+        {
+            FirstNumber = SecondNumber = Result = 0;
+        }
+    }
+
+    public interface ICalculator
+    {
+        int FirstNumber { get; set; }
+
+        int SecondNumber { get; set; }
+
+        int Result { get; set; }
+
+        void Add();
+
+        void Subtract();
+
+        void Multiply();
+
+        void Divide();
+
+        void CleanAll();
+    }
+}
---
+++ SampleLibrarySpecFlowProject/Drivers/CalculatorDriver.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleLibrarySpecFlowProject.Drivers
+{
+    public class CalculatorDriver
+    {
+    }
+}
